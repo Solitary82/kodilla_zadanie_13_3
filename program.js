edd5955da3fe7ec136519c5a086b90bf57c9ac3f
Marta Waplak <mwaplak@gmail.com>
2018-03-07 00:21:54 +0100

    Upload task

---
+++ program.js
@@ -0,0 +1,22 @@
+process.stdin.setEncoding('utf-8');
+process.stdin.on('readable', function() {
+    var input = process.stdin.read();
+    if(input !== null) {
+    var instruction = input.toString().trim();
+    
+        switch(instruction) {
+            case '/exit':
+                process.stdout.write('Quitting app!\n');
+                process.exit();
+                break;
+            case 'env':
+                console.log(process.env.OS);
+                break;
+            case 'version':
+                console.log(process.versions);
+                break;
+            default:
+                process.stderr.write('Wrong instruction!\n');
+        }
+    }
+});
